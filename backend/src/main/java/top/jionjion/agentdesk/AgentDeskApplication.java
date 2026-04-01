c79f3386cddfcd5064b038c85b0314ce4f4ddd7a
jionjion <jionjion@aliyun.com>
2026-04-01 21:42:07 +0800

    - 初始化提交

---
+++ backend/src/main/java/top/jionjion/agentdesk/AgentDeskApplication.java
@@ -0,0 +1,16 @@
+package top.jionjion.agentdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ */
+@SpringBootApplication
+public class AgentDeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentDeskApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/top/jionjion/agentdesk/AgentDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package top.jionjion.agentdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentDeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
